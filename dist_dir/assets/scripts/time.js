33dfe768fc6c1ea537205c4e7c41104673a2fd9b
Jasmine <67263692+jasm1nii@users.noreply.github.com>
2023-08-01 08:51:40 +0700

    Add files via upload

---
+++ dist_dir/assets/scripts/time.js
@@ -0,0 +1,35 @@
+function dateToText(date) {
+    var hours = date.getHours()
+    var minutes = date.getMinutes();
+    // var seconds = date.getSeconds();
+    if (minutes < 10) minutes = '0'+minutes;
+    //if  seconds < 10) seconds = '0'+seconds;
+    if (hours < 10) hours = '0'+hours;
+    return hours + ":" + minutes; // + ":" + seconds;
+}
+function updateClocks() {
+	for (var i = 0; i < window.arrClocks.length; i++) {
+		var clock = window.arrClocks[i];
+		var offset = window.arrOffsets[i];
+		clock.innerHTML = dateToText(new Date(new Date().getTime()+offset));
+	}
+}
+function startClocks() {
+	clockElements = document.getElementsByClassName('clock');
+	window.arrClocks = []
+	window.arrOffsets = [];
+	var j = 0;
+	for(var i = 0; i < clockElements.length; i++) {
+		el = clockElements[i];
+		timezone = parseInt(el.getAttribute('timezone'));
+		if (!isNaN(timezone)) {
+			var tzDifference = timezone * 60 + (new Date()).getTimezoneOffset();
+			var offset = tzDifference * 60 * 1000;
+			window.arrClocks.push(el);
+			window.arrOffsets.push(offset);
+		}
+	}
+	updateClocks();
+	clockID = setInterval(updateClocks, 1000);
+}
+setTimeout(startClocks, 100);
