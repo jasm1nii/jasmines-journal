4033863f3e4d79e95f2e72c4e588469d7c9e4530
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-08-07 05:21:01 +0700

    fixed and added stuff

--- public/changelog/footer.js
+++ public/changelog/footer.js
@@ -8,7 +8,7 @@ document.getElementById("footer").innerHTML =`
 <ul>
     <li><time>2023</time> - until hell freezes over</li>
     <li>by <a href="https://neocities.org/site/jasm1nii" rel="author me">jasm1nii on neocities</a></li>
-    <li><a href="site-map.html">site map</a></li>
-    <li><a href="credits.html">credits</a></li>
+    <li><a href="../site-map.html">site map</a></li>
+    <li><a href="../credits.html">credits</a></li>
 </ul>
 `;
