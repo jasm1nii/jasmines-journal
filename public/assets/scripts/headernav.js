5b8e490f68d3b2a39c9b6e2011ab905a42a4c75b
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-08-06 23:03:33 +0700

    HELL YEAH JAVASCRIPT

---
+++ public/assets/scripts/headernav.js
@@ -0,0 +1,12 @@
+document.getElementById("headernav").innerHTML =`
+<ul>
+    <li><b><a href="index.html">home</a></b></li>
+    <li><a href="about.html">about</a></li>
+    <li>🔨 <del>blog</del></li>
+    <li>🔨 <del>shrines</del></li>
+    <li>🔨 <del>art</del></li>
+    <li>🔨 <del>resources</del></li>
+    <li><a rel="me external" href="https://jasm1nii.123guestbook.com/">guestbook</a></li>
+    <li><a href="neocities-network.html">neocities network</a></li>
+</ul>
+`;
