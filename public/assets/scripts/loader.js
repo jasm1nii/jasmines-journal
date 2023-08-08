308083eefb61379bf032b98b566dab4c49668376
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-08-08 18:28:19 +0700

    added page loader

---
+++ public/assets/scripts/loader.js
@@ -0,0 +1,24 @@
+document.getElementById("loader").innerHTML = `loading contents...
+<style>
+#loader {
+  text-align: center;
+  margin: auto;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 50%;
+  font-size: 1.5em;
+  letter-spacing: .1em;
+  background: thistle;
+  color: indigo;
+}
+</style>`;
+
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
