1ff2949367f36d585dc01ba3a500d19cccf49f33
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-08-14 22:22:26 +0700

    new stylesheet, URL rewrites, more resources

--- public/assets/scripts/headernav-level2.js
+++ public/assets/scripts/headernav-level2.js
@@ -1,11 +1,11 @@
 document.getElementById("headernav").innerHTML =`
 <ul>
-    <li><a href="../index.html">home</a></li>
-    <li><a href="../about/index.html">about</a></li>
-    <li><a href="../blog/index.html">blog</a></li>
+    <li><a href="../">home</a></li>
+    <li><a href="../about/">about</a></li>
+    <li><a href="../blog/">blog</a></li>
     <li>🔨 <del>shrines</del></li>
     <li>🔨 <del>art</del></li>
-    <li><a href="../resources/index.html">resources</a></li>
+    <li><a href="../resources/">resources</a></li>
     <li><a rel="me external" href="https://jasm1nii.123guestbook.com/">guestbook</a></li>
     <li><a href="../link-gallery.html">link gallery</a></li>
 </ul>
