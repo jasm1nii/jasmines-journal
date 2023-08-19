1365b97e509bc245e3e60f3d8cdf770fb151fcb7
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-08-19 21:23:42 +0700

    revised loader positioning

--- public/assets/scripts/loader.js
+++ public/assets/scripts/loader.js
@@ -2,15 +2,17 @@ document.getElementById("loader").innerHTML = `loading contents...
 <style>
 #loader {
   text-align: center;
-  position: absolute;
-  width: 100vw;
-  min-height: 100vh;
+  position: sticky;
+  margin-top: auto;
+  width: 100%;
+  height: 100vh;
   font-size: 1.5em;
   letter-spacing: .1em;
   line-height: 50vh;
   color: white;
   background-color: rgba(83, 68, 117, 1);
   top: 0;
+  overflow-x: hidden;
 }
 </style>`;
 
