25ea20a7da9f7a98d053404ee289a0b5009aca27
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-08-20 15:54:15 +0700

    directory name change, CSS cleanup

--- public/assets/scripts/headernav.js
+++ public/assets/scripts/headernav.js
@@ -3,11 +3,11 @@ document.getElementById("headernav").innerHTML =`
     <li><a href="/">home</a></li>
     <li><a href="/about">about</a></li>
     <li><a href="/blog">blog</a></li>
+    <li>🔨 <del>media library </del></li>
     <li>🔨 <del>shrines</del></li>
     <li>🔨 <del>art</del></li>
     <li><a href="/resources">resources</a></li>
     <li><a href="link-gallery.html">link gallery</a></li>
-    <li>🔨 <del>extras</del></li>
     <li><a rel="me external" href="https://jasm1nii.123guestbook.com/">guestbook</a></li>
 </ul>
 `;
