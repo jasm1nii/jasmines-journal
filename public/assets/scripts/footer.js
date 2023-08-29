77cc37ea1e6eef4b41fa7a3d332b5cbdde0d94b0
Jasmine Amalia <jasm1nii@proton.me>
2023-08-29 16:54:20 +0700

    updated site map, credits, footer
    Signed-off-by: Jasmine Amalia <jasm1nii@proton.me>

--- public/assets/scripts/footer.js
+++ public/assets/scripts/footer.js
@@ -4,19 +4,29 @@ document.querySelector("#footer").innerHTML =`
 </p>
 
 <ul>
-    <li><time>2023</time> - until hell freezes over</li>
-    <li><a href="/about#contact">contact</a></li>
-    <li><a href="/site-map.html">site map</a></li>
-    <li><a href="/credits.html">credits</a></li>
-    <li>🔨 <del>accessibility</del></li>
-    <li>hosted by <a href="https://www.rumahweb.com/" rel="sponsored">rumahweb</a></li>
-    <li>
+    <li id="footerCopyright">
+        <time>2023</time> - until hell freezes over
+    </li>
+    <li id="footerIndieweb">
         <a href="https://indieweb.org/">
         <img src="/assets/media/main/indieweb-badge.png" width="80" height="15" alt="part of the indie web" style="image-rendering:pixelated;"></a>
     </li>
-    <li>
+    <li id="footerWebmentions">
         <a href="https://indieweb.org/Webmention">
         <img src="/assets/media/main/webmention-button.png" width="80" height="15" alt="supports webmentions" style="image-rendering:pixelated;"></a>
+    </li>
+    <li id="footerAcccessibility">
+        🔨 <del>accessibility</del>
+    </li>
+    
+    <li id="footerSitemap">
+        <a href="/site-map.html">site map</a>
+    </li>
+    <li id="footerCredits">
+        <a href="/credits.html">credits</a>
+    </li>
+    <li id="footerContact">
+        <a href="/about#contact">contact</a>
     </li>
 </ul>
 <style>
