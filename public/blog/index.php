5a3275f0341a4da25319bbc8b989cb27d63986d8
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-10-16 22:40:58 +0700

    rewrite horribly convoluted PHP
    Signed-off-by: Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>

--- public/blog/index.php
+++ public/blog/index.php
@@ -1,40 +1,99 @@
 <?php
-    // import notes.xml
-    $notes_xml_source = file_get_contents("notes/notes.xml", FILE_USE_INCLUDE_PATH);
-    $notes_xml = new DOMDocument('notes_source');
-    $notes_xml->loadXML($notes_xml_source);
-
-    // get information from child elements of first feed entry.
-    $notes_title = $notes_xml->getElementsByTagName("title")->item(1);
-    $notes_url = $notes_xml->getElementsByTagName("id")->item(1);
-    $notes_content = $notes_xml->getElementsByTagName("content")->item(0);
-
-    // import blog index template.
-    $blog_index = file_get_contents("blog_index_template.html", FILE_USE_INCLUDE_PATH);
-    $main_dom = new DOMDocument('main');
-    // force libxml to ignore errors from parsing valid HTML 5 markup; PHP's DOM manipulator will throw a hissy fit if you don't, because it only recognizes HTML 4 elements.
-    libxml_use_internal_errors(true);
-    $main_dom->loadHTML($blog_index, LIBXML_NOERROR);
-
-    // import children of feed entry into blog index DOM, then return their text values.
-    $notes_title = $main_dom->importNode($notes_title, true);
-    $notes_title = new DOMText($notes_title->textContent);
-    $notes_url = $main_dom->importNode($notes_url, true)->nodeValue;
-    $notes_content = $main_dom->importNode($notes_content, true)->nodeValue;
-
-    // fill in post title.
-    $notes_title_dom = $main_dom->getElementById('latest-note-title');
-    $notes_title_dom->appendChild($notes_title);
-
-    // link to post within title.
-    $notes_title_dom->setAttribute('href', $notes_url);
-
-    // fill in post content.
-    $notes_content_dom = $main_dom->getElementById('latest-note-content');
-    // this will render HTML entities as their corresponding tags (e.g, > as &gt;), if any markup is present.
-    $p_inner = $main_dom->createDocumentFragment();
-    $p_inner->appendXML($notes_content);
-    $notes_content_dom->appendChild($p_inner);
-
-    echo $main_dom->saveHTML();
+    $notes_xml = file_get_contents(__DIR__.'/notes/notes.xml');
+    $notes_doc = new DOMDocument();
+    $notes_doc->loadXML($notes_xml);
 ?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <?php include  __DIR__.'/../assets/includes/head.shtml'?>
+    <title>jasmine's b(rain)log</title>
+    <meta name="description" content="my web of thoughts" />
+    <meta name="author" content="jasm1nii" />
+    <meta name="keywords" content="blog" />
+    <link href="/assets/stylesheets/style-10.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+<body>
+    <div id="layout">
+        <div class="sidebar">
+            <header>
+                <hgroup>
+                    <h1>jasmine's b(rain)log</h1>
+                </hgroup>
+                <nav>
+                    <?php include __DIR__.'/../assets/includes/headernav.shtml'?>
+                </nav>
+            </header>
+        </div>
+        <main>
+            <article class="microblog">
+                <section>
+                    <hgroup>
+                        <h2>notes</h2>
+                    </hgroup>
+                    <div id="notes">
+                        <article>
+                            <div>
+                                <img src="/assets/media/main/oingo-boingo.png" alt="profile picture" class="u-photo" />
+                            </div>
+                            <div>
+                                <h3 class="p-name">
+                                    <time datetime="
+                                    <?php
+                                        $notes_pub = $notes_doc->getElementsByTagName('published')->item(0)->textContent;
+                                        echo $notes_pub;
+                                    ?>
+                                    "><a href="
+                                    <?php
+                                        $notes_link = $notes_doc->getElementsByTagName('id')->item(1)->textContent;
+                                        echo $notes_link;
+                                    ?>">
+                                        <?php
+                                            $notes_title = $notes_doc->getElementsByTagName('title')->item(1)->textContent;
+                                            echo $notes_title;
+                                        ?>
+                                    </a></time>
+                                </h3>
+                                <p id="latest-note-content" class="e-content">
+                                    <?php
+                                        $notes_content = $notes_doc->getElementsByTagName('content')->item(0)->textContent;
+                                        echo $notes_content;
+                                    ?>
+                                </p>
+                            </div>
+                        </article>
+                    </div>
+                </section>
+                <section class="subscribe">
+                    <p>
+                        <a href="/blog/notes/">view archive</a> / subscribe via <a href="/blog/notes/notes.xml">atom</a>
+                    </p>
+                </section>
+            </article>
+            <article>
+                <section>
+                    <hgroup>
+                        <h2>articles</h2>
+                    </hgroup>
+                    <div id="articlesArchive"></div>
+                </section>
+                <section class="subscribe">
+                    <p>
+                        <a href="/blog/articles/">view archive</a> / subscribe via <a
+                            href="/blog/articles/articles.xml">atom</a>
+                    </p>
+                </section>
+            </article>
+        </main>
+    </div>
+    <footer>
+        <?php include  __DIR__.'/../assets/includes/footer.shtml'?>
+    </footer>
+    <script src="index.js"></script>
+    <script src="/assets/scripts/articles-archive.js"></script>
+    <script src="/assets/scripts/theme-switcher-v2.js"></script>
+</body>
+</html>
