105e81bfcfcd035ce2c53220a6dc2acbad914b0d
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-10-23 23:33:05 +0700

    rename directories
    Signed-off-by: Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>

--- tests/link-gallery_pug.php
+++ tests/link-gallery_pug.php
@@ -1,5 +1,5 @@
 <?php
     include_once dirname(__DIR__,1) . '/config/pug_default_config.php';
     $output = __DIR__.'/link-gallery_pug.html';
-    Phug::renderAndWriteFile(dirname(__DIR__,1).'/data/link-gallery_pug_src.pug', $output);
+    Phug::renderAndWriteFile(dirname(__DIR__,1).'/resources/content/link-gallery/link-gallery_buttons.pug', $output);
 ?>
