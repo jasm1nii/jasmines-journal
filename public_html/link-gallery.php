342fe2e3de9238691a2d889d846a7f50313968ff
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-10-24 12:39:50 +0700

    fix link gallery buttons

---
+++ public_html/link-gallery.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <?php include_once dirname(__DIR__,1).'/resources/includes/head.shtml'; ?>
+    <title>link gallery | jasmine's journal</title>
+    <meta name="description" content="a list of cool websites on the indie web"/>
+    <link rel="canonical" href="https://jasm1nii.xyz/link-gallery"/>
+    <link href="/_assets/stylesheets/link-gallery.css" rel="stylesheet" type="text/css" media="all"/>
+  </head>
+
+  <body>
+    <div class="board">
+      <header>
+        <div class="header-top">
+          <hgroup>
+            <h1>jasmine's link gallery</h1>
+            <p>
+              page contents updated
+              <time datetime="2023-10-09">9 october 2023</time>
+            </p>
+          </hgroup>
+        </div>
+        <nav>
+          <?php include_once dirname(__DIR__,1).'/resources/includes/headernav.shtml'; ?>
+        </nav>
+      </header>
+
+      <main>
+        <article>
+          <div class="page-columns">
+
+            <section>
+              <div class="card warning">
+                <h2 id="warning">photosensitivity warning</h2>
+                <p>
+                  animated GIFs are paused by default -
+                  <strong>toggle at your own discretion ✨</strong>
+                </p>
+                <div id="gifControls">
+                  <button id="play-gif" type="button">Play GIFs</button>
+                  <button id="stop-gif" type="button">Stop GIFs</button>
+                </div>
+              </div>
+            </section>
+
+            <section>
+              <div class="card buttons">
+                <h2 id="web-friends">web friends</h2>
+                <p>
+                  mutual linkers and followers from various places on the <a href="https://indieweb.org/" rel="external">indie web</a>!
+                </p>
+                <div class="button-gallery">
+                  <span class="freezeframe">
+                    <?php include dirname(__DIR__,1).'/resources/content/link-gallery/link-gallery_mutuals.html';?>
+                  </span>
+                </div>
+                <hr>
+                <p>
+                  <small>
+                    if you haven't been listed yet - don't hesitate to <a href="/about#contact">contact me.<a>
+                  </small>
+                </p>
+              </div>
+            </section>
+
+            <section>
+              <div class="card buttons">
+                <h2 id="cafegoers">32-bit cafegoers</h2>
+                <p>
+                  join the community on <a href="https://32bit.cafe/discord/" rel="external">discord</a>! (<b>18+ only</b>, SFW)
+                  <br>
+                  <a href="https://32bit.cafe/" rel="external"><img src="/_assets/media/link-gallery/32b-win9x.png" alt="32-bit cafe" width="88" height="31" /></a>
+                </p>
+                <hr>
+                <div class="button-gallery">
+                  <span class="freezeframe">
+                    <?php include dirname(__DIR__,1).'/resources/content/link-gallery/link-gallery_32bitcafe.html';?>
+                  </span>
+                </div>
+              </div>
+            </section>
+
+            <section>
+              <div class="card buttons">
+                <h2 id="more-cool-sites">more cool sites</h2>
+                <div class="button-gallery">
+                  <span class="freezeframe">
+                    <a href="https://dimden.dev/" rel="external"><img src="/_assets/media/link-gallery/dimden.gif" alt="dimden's hotel on neocities" width="88" height="31" /></a>
+                    <a href="https://debtdeath.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/debtdeath-2-button.png" alt="debtdeath on neocities" width="88" height="31" /></a>
+                    <a href="https://adilene.net/" rel="external"><img src="/_assets/media/link-gallery/adilene-sitebutton.png" alt="virtual diva on neocities" width="88" height="31" /></a>
+                    <a href="https://lalala-doomsday.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/starry_button.gif" alt="doomsday-bird on neocities" width="88" height="31" /></a>
+                    <a href="https://solinus.neocities.org" rel="external"><img src="/_assets/media/link-gallery/solinus.png" alt="solinus on neocities" width="88" height="31" /></a>
+                    <a href="https://cinnamuff.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/cinnamuffbtn.png" alt="cinnamuff on neocities" width="88" height="31" /></a>
+                    <a href="https://cloverbell.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/cloverbell.gif" alt="cloverbell on neocities" width="88" height="31" /></a>
+                    <a href="https://fearoffun.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/fearoffun.gif" alt="fearoffun" width="88" height="31" /></a>
+                    <a href="https://starrs.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/tiny_space.gif" alt="a tiny space on neocities" width="88" height="31" /></a>
+                    <a href="https://gnomes.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/gnomes.gif" alt="Gnome Watch" width="88" height="31" /></a>
+                    <a href="https://bechnokid.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/bechnokid.jpg" alt="bechnokid on neocities" width="88" height="31" /></a>
+                    <a href="https://cherubiims.neocities.org" rel="external"><img src="/_assets/media/link-gallery/cherubbutton.gif" alt="button for cherubiims on neocities" width="88" height="31" /></a>
+                    <a href="https://freakphone.net" rel="external"><img src="/_assets/media/link-gallery/freakbutton2.gif" alt="button for freak phone on neocities" width="88" height="31" /></a>
+                    <a href="https://www.bikobatanari.art/" rel="external"><img src="/_assets/media/link-gallery/bikobatanari.gif" alt="button for bikobatanari on neocities" width="88" height="31" /></a>
+                    <a href="https://irony-machine.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/irony-machine.png" alt="button for irony machine on neocities" width="88" height="31" /></a>
+                    <a href="https://aegi.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/aegi_button.gif" alt="button for aegi on neocities" width="88" height="31" /></a>
+                    <a href="https://fuchi.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/fuchibutton.jpeg" alt="button for fuchi on neocities" width="88" height="31" /></a>
+                    <a href="https://chaodamacy.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/chaodamacy.png" alt="button for chaodamacy on neocities" width="88" height="31" /></a>
+                    <a href="https://crowaes.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/waterfallbuttonpurple.png" alt="crowaes (formerly waterfall) on neocities" width="88" height="31" /></a>
+                    <a href="https://nenrikido.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/nenrikido_button.gif" alt="button for nenrikido on neocities" width="88" height="31" /></a>
+                    <a href="https://maya.land/" rel="external"><img src="/_assets/media/link-gallery/maya dot land.png" alt="maya land on neocities" width="88" height="31" /></a>
+                    <a href="https://macchiato.neocities.org" rel="external"><img src="/_assets/media/link-gallery/macchiato.png" alt="button for macchiato on neocities" width="88" height="31" /></a>
+                    <a href="https://shishka.neocities.org/" rel="external"><img src="/_assets/media/link-gallery/shishka-ulala.gif" alt="button for shiska on neocities" width="88" height="31" /></a>
+                    <a href="https://sadgrl.online/" rel="external"><img src="/_assets/media/link-gallery/sadgrlonline.gif" alt="button for sadgrl.online" width="88" height="31" /></a>
+                    <a href="https://entranceorexit.net/" rel="external"><img src="/_assets/media/link-gallery/eoe_button.png" alt="button for entrance or exit" width="88" height="31" /></a>
+                    <a href="https://oekaki.cinni.net/" rel="external"><img src="/_assets/media/link-gallery/cinni-oekaki-88x31.gif" alt="button for cinni's oekaki" width="88" height="31" /></a>
+                    <a href="https://cinni.net/directory/" rel="external"><img src="/_assets/media/link-gallery/ichigo-directory.png" alt="button for ichigo directory" width="88" height="31" /></a>
+                  </span>
+                </div>
+              </div>
+            </section>
+
+            <section>
+              <div class="card web-garden">
+                <h2 id="web-garden">web garden</h2>
+                <p>
+                  nothing planted for now - in the mean time, check out the <a href="https://webgardens.neocities.org/" rel="external">project page 🌷</a>
+                </p>
+              </div>
+            </section>
+            
+            <section>
+              <div class="card my-button">
+                <h2 id="link-me">link to this site</h2>
+                <p>
+                  <img src="/_assets/media/main/jasmines-journal-button.png" alt="jasmine's journal" width="88" height="31" loading="lazy"/>
+                  <br>
+                  copy and paste the following code into your HTML file ↓
+                </p>
+                <div class="code" tabindex="0">
+                  <code>
+                    &lt;a href="https://jasm1nii.xyz/" rel="external">&lt;img src="https://jasm1nii.xyz/_assets/media/main/jasmines-journal-button.png" alt="jasmine's journal" style="outline: 1px solid gray; image-rendering: pixelated;" width="88" height="31" />&lt;/a>
+                  </code>
+                </div>
+                <p>
+                  <small>hotlinking is fine, but self-hosting the image is recommended.</small>
+                </p>
+              </div>
+            </section>
+
+          </div>
+        </article>
+      </main>
+    </div>
+
+    <footer>
+      <?php include dirname(__DIR__,1).'/resources/includes/footer.shtml';?>
+    </footer>
+
+    <script src="/_assets/scripts/theme-switcher-v2.js"></script>
+    <script src="/_assets/scripts/freezeframe.js"></script>
+  </body>
+</html>
--- src/vendor/composer/installed.php
+++ src/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'dd0912fe986829eb772a9ca23a9ed9beec888517',
+        'reference' => 'e22f197adc129bc07b7dd54819261ecd4ec4f20d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'dd0912fe986829eb772a9ca23a9ed9beec888517',
+            'reference' => 'e22f197adc129bc07b7dd54819261ecd4ec4f20d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
