f3db5c720645c9db40f33f8083f8eeef00ad63ca
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-10-28 19:56:32 +0700

    fix clock css jank

--- public_html/index.js
+++ public_html/index.js
@@ -128,10 +128,10 @@ const surprise = document.getElementById('surprise');
   function dateToText(date) {
     var hours = date.getHours();
     var minutes = date.getMinutes();
-    // var seconds = date.getSeconds();
-    if (minutes < 10) minutes = '0'+ minutes;
-    //if  seconds < 10) seconds = '0'+seconds;
-    if (hours < 10) hours = '0'+ hours;
+    //var seconds = date.getSeconds();
+    if (minutes < 10) minutes = '0' + minutes;
+    //if  (seconds < 10) seconds = '0'+seconds;
+    if (hours < 10) hours = '0' + hours;
     return innerHTML = hours + `:` + minutes; // + ":" + seconds;
   };
   function updateClocks() {
