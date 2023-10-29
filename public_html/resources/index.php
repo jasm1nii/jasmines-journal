9b572efc7ee62a4c357b340ea02a819f2229fef3
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-10-29 15:15:38 +0700

    prep resource page templates

--- public_html/resources/index.php
+++ public_html/resources/index.php
@@ -1,5 +1,7 @@
 <?php
     require_once dirname(__DIR__,2).'/config/twig_default_config.php';
 
-    echo $twig->render('/resources/layouts/resources/resources_index.html.twig');
+    $md_source = filemtime(dirname(__DIR__,2).'/resources/content/resources/resources_index.md');
+
+    echo $twig->render('/resources/layouts/resources/resources_index.html.twig',['updated'=>$md_source]);
 ?>
---
+++ resources/content/resources/_toc.md
@@ -0,0 +1,9 @@
+## table of contents
+- [emoji legend](#emoji-legend)
+- **main sections**
+    1. [art & design](#art-design)
+    1. [organization & productivity](#organization-productivity)
+    1. [web development](#web-development)
+    1. [blogging platforms](#blogging-platforms)
+    1. [neurodiversity](#neurodiversity)
+    1. [miscellaneous](#miscellaneous)
