1c8697386386707c30b7247050eae86663c0f136
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-11-01 13:54:53 +0700

    update feed generator script

--- bin/feed-generator_v2/notes_xml_generator.php
+++ bin/feed-generator_v2/notes_xml_generator.php
@@ -1,29 +1,34 @@
 <?php
-    require_once dirname(__DIR__,2).'/config/twig_default_config.php';
+    $server_root = dirname(__DIR__,2);
+    require_once $server_root.'/config/twig_default_config.php';
 
-    $source = dirname(__DIR__,2).'/resources/content/blog/notes';
-    $layout = "/src/feed-generator_v2/notes_entry.xml.twig";
+    $source = $server_root.'/resources/content/blog/notes';
+    $layout = "/bin/feed-generator_v2/notes_entry.xml.twig";
 
     $files = glob($source."/*/*/*/entry.html.twig");
     asort($files, SORT_NATURAL);
     
     ob_start();
     foreach (array_reverse($files) as $article) {
-        $content_path = ltrim($article,dirname(__DIR__,2));
+        $path = ltrim($article, $server_root);
+        $slug = ltrim(rtrim($path,'.html.twig'),'/resources/content/blog/notes');
 
-        $slug_1 = rtrim($content_path,'.html.twig');
-        $slug_2 = ltrim($slug_1,'/resources/content/blog/notes');
-        $media_source = 'https://jasm1nii.xyz/_assets/media/blog/articles/'.rtrim($slug_2,'/entry').'/';
-
-        echo $twig->render($content_path, ['layout'=>$layout,'slug'=>$slug_2,'src'=>$media_source]);
+        echo $twig->render($path, ['layout'=>$layout,'slug'=>$slug]);
     }
+
+    $target_dir = $server_root.'/tests';
     $xml_entries = ob_get_contents();
-    file_put_contents(__DIR__.'/notes.tmp.xml',$xml_entries);
+
+    file_put_contents($target_dir.'/notes.tmp.xml', $xml_entries);
+
     ob_end_clean();
 
     ob_start();
-    echo $twig->render('/src/feed-generator_v2/notes.xml.twig');
+
+    echo $twig->render('/bin/feed-generator_v2/notes.xml.twig');
     $xml_final = ob_get_contents();
-    file_put_contents(__DIR__.'/notes.xml',$xml_final);
+
+    file_put_contents($target_dir.'/notes.xml', $xml_final);
+
     ob_end_clean();
 ?>
