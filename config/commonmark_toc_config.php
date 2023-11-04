74e82b3e9cf10af6f2f2fe6ec481ae83edf19f3b
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-11-04 12:55:18 +0700

    fix broken ToC links

--- config/commonmark_toc_config.php
+++ config/commonmark_toc_config.php
@@ -14,8 +14,8 @@
     $commonmark_config = [
         'heading_permalink' => [
             'html_class' => 'heading-permalink',
-            'id_prefix' => 'content',
-            'apply_id_to_heading' => false,
+            'id_prefix' => '',
+            'apply_id_to_heading' => true,
             'heading_class' => '',
             'fragment_prefix' => '',
             'insert' => 'before',
