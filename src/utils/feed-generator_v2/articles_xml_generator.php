c26a4ba1015da53eb21c83143b993b6934ba8eb6
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-11-11 21:02:34 +0700

    update composer.json, change paths

--- src/utils/feed-generator_v2/articles_xml_generator.php
+++ src/utils/feed-generator_v2/articles_xml_generator.php
@@ -1,20 +1,30 @@
 <?php
-    $server_root = dirname(__DIR__,2);
-    require_once $server_root.'/config/twig_default_config.php';
+    $server_root = dirname(__DIR__,3);
+    require_once $server_root . '/vendor/autoload.php';
+    require_once $server_root.'/src/utils/twig_default_config.php';
 
     $source_dir = $server_root.'/resources/content/blog/articles';
-    $layout = "/bin/feed-generator_v2/articles_entry.xml.twig";
+    $layout = "/src/utils/feed-generator_v2/articles_entry.xml.twig";
 
     $files = glob($source_dir."/*/*/*/entry.html.twig");
     asort($files, SORT_NATURAL);
+
+    //
     
     ob_start();
     foreach (array_reverse($files) as $article) {
         $path = ltrim($article, $server_root);
-        $slug = ltrim(rtrim($path,'.html.twig'),'/resources/content/blog/articles');
+        $slug = ltrim(rtrim($path,'.html.twig'),'/src/content/blog/articles');
         $media_src = "https://jasm1nii.xyz/_assets/media/blog/articles/".rtrim($slug, "entry");
 
-        echo $twig->render($path, ['layout'=>$layout,'slug'=>$slug, 'src'=>$media_src]);
+        echo $twig->render(
+            $path,
+            [
+                'layout'=>$layout,
+                'slug'=>$slug,
+                'src'=>$media_src
+            ]
+        );
     }
 
     $target_dir = $server_root.'/tests';
@@ -24,9 +34,11 @@
 
     ob_end_clean();
 
+    //
+
     ob_start();
 
-    echo $twig->render('/bin/feed-generator_v2/articles.xml.twig');
+    echo $twig->render('/src/utils/feed-generator_v2/notes.xml.twig');
     $xml_final = ob_get_contents();
 
     file_put_contents($target_dir.'/articles.xml', $xml_final);
