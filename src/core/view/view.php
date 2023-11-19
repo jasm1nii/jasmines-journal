4415a6f8f1646a461c04e464d50271002782f926
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-11-19 13:18:27 +0700

    remove leftover consts/require functions

--- src/core/view/view.php
+++ src/core/view/view.php
@@ -4,16 +4,7 @@
 
     class View {
 
-        const DIR = __DIR__ . '/extensions/';
-        const TWIG = self::DIR . "twig_global.php";
-        const TWIG_PARTIAL = self::DIR . "twig_partial.php";
-        const MARKDOWN_COMMENTS = self::DIR  . "commonmark_comments.php";
-        const MARKDOWN_WITH_TOC = self::DIR  . "commonmark_toc.php";
-        const UTILS = self::DIR . "utils.php";
-
         public static function Twig($page, $vars, $path = null) {
-
-            require self::TWIG;
 
             $twig = new Extension\Twig();
 
