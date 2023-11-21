7dd58562459a7e5d69277e80f97f38063a07743e
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-11-21 18:27:02 +0700

    edit twig env for partials, fix blog layouts

--- src/core/view/extensions/twig_partial.php
+++ src/core/view/extensions/twig_partial.php
@@ -4,6 +4,9 @@
 
     use Twig\Extra\Intl\IntlExtension;
     use Twig\RuntimeLoader\RuntimeLoaderInterface;
+    use Twig\Extra\Markdown\DefaultMarkdown;
+    use Twig\Extra\Markdown\MarkdownExtension;
+    use Twig\Extra\Markdown\MarkdownRuntime;
 
     class PartialTwig {
 
@@ -20,6 +23,17 @@
             );
 
             $twig->addExtension(new IntlExtension());
+            $twig->addExtension(new MarkdownExtension());
+            $twig->addRuntimeLoader(
+                new class () implements RuntimeLoaderInterface {
+                    public function load($class)
+                    {
+                        if (MarkdownRuntime::class === $class) {
+                            return new MarkdownRuntime(new DefaultMarkdown());
+                        }
+                    }
+                }
+            );
             $twig->getExtension(\Twig\Extension\CoreExtension::class)->setDateFormat(DATE_ATOM);
             $twig->getExtension(\Twig\Extension\CoreExtension::class)->setTimezone('Asia/Jakarta');
 
