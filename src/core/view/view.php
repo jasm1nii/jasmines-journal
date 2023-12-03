b30ae27a0c53cfe54eeabf4e91f8d79d87e62635
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-12-03 10:18:02 +0700

    fix view type hints

--- src/core/view/view.php
+++ src/core/view/view.php
@@ -4,11 +4,11 @@
 
     class View {
 
-        public static function Twig(string $page, array $vars, string $path = null) {
+        public static function Twig(string $page, array|null $vars, string $path = null) {
 
             $twig = new Extension\Twig();
 
-            $vars ?? [];
+            $vars ?? $vars = [];
 
             $loader = $twig->loadBaseLoader($path);
             $twig->createEnvAndMake($loader, $page, $vars);
