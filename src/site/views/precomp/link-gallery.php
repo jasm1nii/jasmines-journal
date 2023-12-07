cdd8b6154f44083e345d199d9fbecb99810c9275
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-12-07 14:08:02 +0700

    remove utility classes, group themes

--- src/site/views/precomp/link-gallery.php
+++ src/site/views/precomp/link-gallery.php
@@ -11,40 +11,13 @@
 
         protected static string $includes_path = DIR['content'] . "link-gallery";
         
-        private array $includes;
-
-        final public function __construct() {
-
-            $this->getIncludes();
-            $this->render();
-
-        }
-
-        private function getIncludes(): void {
-
-            $this->includes = [
-                'mutuals'
-                    => self::$includes_path . "/link-gallery_mutuals.html.twig",
-
-                '32bit'
-                    => self::$includes_path . "/link-gallery_32bitcafe.html.twig",
-
-                'etc'
-                    => self::$includes_path . "/link-gallery_other-sites.html.twig"
-            ];
-
-        }
-
         final protected function render(): void {
 
             $vars = [
                 'updated'   => Route::getLastUpdated(SITE_ROOT . self::$includes_path . "/*"),
-                'mutuals'   => $this->includes['mutuals'],
-                '_32bit'    => $this->includes['32bit'],
-                'etc'       => $this->includes['etc']
             ];
 
-            parent::Twig($this->layout, $vars, null);
+            parent::Twig($this->layout, $vars, self::$includes_path);
 
         }
 
