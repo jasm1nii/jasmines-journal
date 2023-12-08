67a37108f763edb926bfd8d356cee0c5cbb8780f
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-12-08 13:03:37 +0700

    split home stylesheet into smaller parts

--- public_html/_assets/scripts/theme-switcher-v2.js
+++ public_html/_assets/scripts/theme-switcher-v2.js
@@ -1,41 +1,63 @@
 // based on https://gist.github.com/qaisarferoz/03b133e7fc535dd9f097e31d1f2d1303
 
-// retains user-selected theme independent of system color preferences
+{
+    var baseTheme = document.getElementById("base-theme").getAttribute("href");
+    var baseThemeFile = baseTheme.split("/")[3];
+    var baseThemeName = baseThemeFile.replace(".css","");
+
+    if(!document.getElementById('js-theme')) {
+        var link = document.createElement('link');
+            link.id = 'js-theme';
+            link.rel = 'stylesheet';
+            link.href = `/_assets/stylesheets/${baseThemeName}_theme.css`;
+            link.type = 'text/css';
+        document.head.appendChild(link);
+    }
+
+};
+
+// retains user-selected theme independent of system color preferences:
+
 window.matchMedia('(prefers-color-scheme: dark)')
     .addEventListener('change', event => {
         event.matches ? "dark" : "light";
 });
-// creates theme toggle button
+
 const   page = document.body;
 const   themeSwitch = document.createElement('button');
-        themeSwitch.setAttribute('type', 'button');
-        themeSwitch.setAttribute('id', 'themeSwitch');
+
+// creates theme toggle button:
+
+themeSwitch.setAttribute('type', 'button');
+themeSwitch.setAttribute('id', 'themeSwitch');
 document.querySelector('hgroup').insertAdjacentElement('beforebegin',themeSwitch);
 
-// set default theme
+// set default theme:
+
 if(localStorage.theme) {
     page.classList.add(localStorage.theme);
-    } 
-    else {
-        page.classList.add('light-mode');
-    };
-// set theme indicator text
-if (page.classList.contains('light-mode')){
-        themeSwitch.innerText = '🌞';
-    }
-    else {
-        themeSwitch.innerText = '🌚';
-    };
-// switch theme and store in local storage
-themeSwitch.addEventListener("click", function(){
+} else {
+    page.classList.add('light-mode');
+};
+
+// set theme indicator text:
+
+if (page.classList.contains('light-mode')) {
+    themeSwitch.innerText = '🌞';
+} else {
+    themeSwitch.innerText = '🌚';
+};
+
+// switch theme and store in local storage:
+
+themeSwitch.addEventListener("click", function() {
     if (page.classList.contains('light-mode')){
         page.classList.remove('light-mode');
         page.classList.add('dark-mode');
         themeSwitch.innerText = '🌚';
         themeSwitch.setAttribute('title','current theme: dark');
         localStorage.theme = 'dark-mode';
-        }
-    else {
+    } else {
         page.classList.remove('dark-mode');
         page.classList.add('light-mode');
         themeSwitch.setAttribute('title','current theme: light');
