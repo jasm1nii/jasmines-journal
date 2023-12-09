06164758eadacb0168f9377d41e548cc60ec6c6d
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-12-09 21:41:47 +0700

    fix z-index

--- public_html/_assets/scripts/loader.js
+++ public_html/_assets/scripts/loader.js
@@ -2,15 +2,19 @@
     var page = document.querySelector(".board");
     var loader = document.querySelector(".loader");
 
-    var loadExit = setTimeout(showExit, 3000);
-
-    function showExit() {
-        var exitButton = document.createElement("button");
-        var exitText = document.createTextNode("show me whatever");
-        loader.appendChild(exitButton);
+    var exitButton = document.createElement("button");
+    var exitText = document.createTextNode("show me whatever");
         exitButton.setAttribute('type', 'button');
         exitButton.appendChild(exitText);
         exitButton.addEventListener("click", closeLoader);
+
+    var loadExit = setTimeout(() => { loader.appendChild(exitButton); }, 3000);
+
+    function closeLoader() {
+        loader.style.display = 'none';
+        loader.remove();
+        page.style.animation = 'fadeInSlide .5s ease-in forwards';
+        return;
     };
 
     var loaderLoop = setInterval(incrLoader, 200);
@@ -44,12 +48,6 @@
             loaderText.replaceWith(fin);
             return;
         };
-    };
-
-    function closeLoader() {
-        loader.style.display = 'none';
-        loader.remove();
-        page.style.animation = 'fadeInSlide .5s ease-in forwards';
     };
 
     window.addEventListener("load", ()=> {
