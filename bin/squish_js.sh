f172f9a0aa103693c55f847376e80805d4c3d1b1
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-12-17 10:13:47 +0700

    fix bash script

--- bin/squish_js.sh
+++ bin/squish_js.sh
@@ -4,10 +4,6 @@ echo "relative path to the uncompressed javascript file (omit the \".js\" extens
 
 read -r file
 
-# this doesn't give me the result i want, so will edit later:
-# source_map_url=$(grep -E "\/|[a-z]+$" "$file")
+source_map_url=$(basename "$file")
 
-terser "public_html/_assets/scripts/$file.js" --compress --mangle --output "public_html/_assets/scripts/$file.min.js"
-
-# append to the end of terser once i figure out how to get the string:
-# --source-map "url='$source_map_url.min.js.map'"
+terser "public_html/_assets/scripts/$file.js" --compress --mangle --output "public_html/_assets/scripts/$file.min.js" --source-map "url='$source_map_url.min.js.map'"
