4a3bde50f442f6758294a842b389f26c437a8d7a
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-12-18 19:31:31 +0700

    update cron script

--- bin/db_update.php
+++ bin/db_update.php
@@ -7,6 +7,12 @@
     use JasminesJournal\Site\Models\NotesDatabase;
     use JasminesJournal\Site\Models\ArticlesDatabase;
 
+    if (PHP_SAPI == 'cli' && getenv('ENV') == 'prod') {
+
+        $_SERVER['HTTP_HOST'] = 'jasm1nii.xyz';
+
+    }
+
     function updateArticles(): void {
 
         $db = new ArticlesDatabase;
