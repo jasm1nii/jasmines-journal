8d7202c3ce9be84292019a245c2fe8e643eb11e8
Jasmine Amalia <67263692+jasm1nii@users.noreply.github.com>
2023-12-26 10:43:19 +0700

    reposition elements

--- public_html/_assets/scripts/home/home.js
+++ public_html/_assets/scripts/home/home.js
@@ -21,7 +21,7 @@
     } else {
       notice.style.display = 'none';
     }
-  };
+  }
 
   document.getElementById("pin").addEventListener("click", showNote);
   
@@ -31,24 +31,30 @@
   const surprise = document.getElementById('surprise');
 
   function createButton() {
-     const flower = document.createElement('img');
-     const flwrAttr = {
+
+    const flower = document.createElement('img');
+    const flwrAttr = {
       src: '/_assets/media/main/flower.svg',
-      width: 60,
-      height: 60,
+      width: 'auto',
+      height: 'auto',
       alt: ''
     };
 
-    for (let attr in flwrAttr) { 
+    for (let attr in flwrAttr) {
       flower.setAttribute(attr, flwrAttr[attr]);
-    };
+    }
+
+    flower.style.width = '100%';
+    flower.style.height = 'auto';
 
     const flwrBtn = document.createElement('button');
+
     flwrBtn.insertAdjacentElement('beforeend', flower);
     flwrBtn.setAttribute('title', 'open for a surprise!');
     flwrBtn.setAttribute('id', 'yipee-toggle');
 
     const fs = flwrBtn.style;
+
     fs.backgroundColor = 'transparent';
     fs.border = 'none';
     fs.padding = 0;
@@ -72,48 +78,65 @@
       class: 'u-featured',
       id: 'creatura',
       loading: 'lazy'
-    };
+    }
 
     for (let attr in creatureAttr) {
       creature.setAttribute(attr, creatureAttr[attr]);
-    };
-
-    const cs = creature.style;
-
-    function calcOffset() {
-      let minLeftOffset = window.innerWidth / 4;
-      let maxLeftOffset = window.innerWidth / 2;
-      cs.left = `clamp(${minLeftOffset}px, 25vw, ${maxLeftOffset}px)`;
     }
-
-    calcOffset();
-    window.addEventListener('resize', calcOffset);
     
-    cs.position = 'absolute';
-    cs.top = '25vh';
-    cs.display = 'block';
-    cs.margin = 'auto';
-    cs.zIndex = '4';
-
     return creature;
 
-  };
+  }
 
   const popup = addCreature();
 
-  function toggleYippee() {
+  popup.style.position = 'fixed';
+  popup.style.display = 'block';
+  popup.style.margin = 'auto';
+  popup.style.zIndex = '4';
+  popup.style.top = `10vh`;
 
-    if (!document.contains(popup)) {
+  function calcOffset() {
 
-      document.body.insertAdjacentElement('afterbegin', popup);
+    let minLeftOffset;
+    let maxLeftOffset;
+
+    if (window.innerWidth > 600) {
+
+      minLeftOffset = window.innerWidth / 3;
+      maxLeftOffset = window.innerWidth / 2;
+      popup.style.left = `clamp(${minLeftOffset}px, 40vw, ${maxLeftOffset}px)`;
+
+    } else if (window.innerWidth > 400) {
+
+      minLeftOffset = window.innerWidth / 5;
+      maxLeftOffset = window.innerWidth / 4;
+      popup.style.left = `clamp(${minLeftOffset}px, 20vw, ${maxLeftOffset}px)`;
+
+    } else if (window.innerWidth > 350) {
+
+      minLeftOffset = window.innerWidth / 7;
+      maxLeftOffset = window.innerWidth / 5;
+      popup.style.left = `clamp(${minLeftOffset}px, 10vw, ${maxLeftOffset}px)`;
 
     } else {
 
-      popup.remove();
-      
+      popup.style.left = '0px';
+
     }
 
-  };
+  }
+
+  calcOffset();
+  window.addEventListener('resize', calcOffset);
+
+  function toggleYippee() {
+    !document.contains(popup)
+      ? document.body.insertAdjacentElement('afterbegin', popup)
+      : popup.remove();
+  }
+
+  button.addEventListener('click', toggleYippee);
 
   let confetti = new Confetti('yipee-toggle');
 
@@ -123,30 +146,42 @@
   confetti.setFade(false);
   confetti.destroyTarget(false);
 
-  button.addEventListener('click', toggleYippee);
+  popup.addEventListener('click', () => {
+    popup.remove();
+  });
 
 };
 
 { // clock
   function dateToText(date) {
+    
     let hours = date.getHours();
     let minutes = date.getMinutes();
-    // var seconds = date.getSeconds();
+    // let seconds = date.getSeconds();
+
     if (minutes < 10) { minutes = '0' + minutes; };
-    // if  (seconds < 10) seconds = '0'+seconds;
+    // if  (seconds < 10) seconds = '0' + seconds;
     if (hours < 10) { hours = '0' + hours; };
+
     return innerHTML = hours + `:` + minutes; // + ":" + seconds;
-  };
+
+  }
 
   function updateClocks() {
+
     for (var i = 0; i < window.arrClocks.length; i++) {
+
       let clock = window.arrClocks[i];
       let offset = window.arrOffsets[i];
-      clock.innerHTML = dateToText(new Date(new Date().getTime()+offset));
-    };
-  };
+
+      clock.innerHTML = dateToText(new Date(new Date().getTime() + offset));
+
+    }
+
+  }
   
   function startClocks() {
+
     window.arrClocks = [];
     window.arrOffsets = [];
 
@@ -154,58 +189,77 @@
     let timezone = parseInt(el.getAttribute('timezone'));
 
     if (!isNaN(timezone)) {
+
       let tzDifference = timezone * 60 + (new Date()).getTimezoneOffset();
       let offset = tzDifference * 60 * 1000;
+
       window.arrClocks.push(el);
       window.arrOffsets.push(offset);
-    };
+
+    }
 
     updateClocks();
     setInterval(updateClocks, 1000);
-  };
+
+  }
 
   setTimeout(startClocks, 100);
 
 };
 
 { // last.fm
+
   const user = 'jasm1nii';
   const url = 'https://lastfm-last-played.biancarosa.com.br/' + user + '/latest-song';
   const song = document.getElementById('song');
 
   try {
+
     fetch(url)
     .then((response) => { return response.json()})
     .then((json) => { song.innerHTML = json['track']['name'] + ' - ' + json['track']['artist']  ['#text']; });
+
   } catch {
+
     song.innerText = "couldn't fetch track 😥";
+
   };
 
   // scrolling animation toggle
   const jstoggle = document.getElementById('js-toggle');
   
   function pauseAnimation() {
+
     const animation = document.querySelector('[data-animation]');
     let state = animation.style.animationPlayState || 'running';
     animation.style.animationPlayState = (state === 'running') ? 'paused' : 'running';
-  };
+
+  }
   
   jstoggle.addEventListener('click', pauseAnimation);
+  
 };
 
 { // status.cafe
+
   const statusContent = document.getElementById("statuscafe-content");
   const statusUser = document.getElementById("statuscafe-username");
+
   try {
+
     fetch("https://status.cafe/users/jasm1nii/status.json")
       .then( r => r.json() )
       .then( r => {
         statusUser.innerHTML = '<a href="https://status.cafe/users/jasm1nii" rel="external me" target="_blank">' + r.author + '</a> ' + r.face + ' ' + r.timeAgo;
         statusContent.innerHTML = r.content;
       });
+
   } catch {
+
     statusContent.innerHTML = "couldn't fetch status 🤐";
-  };
+
+  }
+
 };
 
 { // webrings
@@ -223,6 +277,7 @@
       'https://shinyexe.neocities.org/',
       'https://trinityexe.neocities.org/homepage'
     ];
+
     const resourceRing_ringurl = "https://pixelsafari.neocities.org/webring/";
     const resourceRing_badgeurl = "/_assets/media/main/resourcering.png";
     const resourceRing_prevurl = "/_assets/media/main/resourceringprev.png";
@@ -238,8 +293,11 @@
     let randomID = Math.floor(Math.random() * resourceRing_members.length);
 
     let html = `<span><a href='` + resourceRing_ringurl + `'><img alt='Badge: resourceRing webring' src='` + resourceRing_badgeurl + `' width='88' height='31' loading='lazy'/></a></span>`;
+
     html += `<span><a href='` + resourceRing_members[beforeID] + `'><img alt='Previous' src='` + resourceRing_prevurl + `' height='31' width='22' loading='lazy'/></a>`;
+
     html += `<a href='` + resourceRing_members[randomID] + `'><img alt='Random' src='` + resourceRing_randomurl + `' height='31' width='44' loading='lazy'/></a>`;
+
     html += `<a href='` + resourceRing_members[afterID] + `'><img alt='Next' src='` + resourceRing_nexturl + `' height='31' width='22' loading='lazy'/></a></span>`;
     
     displayElement.innerHTML = html;
@@ -252,7 +310,9 @@
     w9g5p9.innerHTML = `<p class="safonts"></p>`;
 
     class p1v1g6 extends HTMLElement {
+
       connectedCallback() {
+
         this.attachShadow({ mode: "open" });
         this.shadowRoot.appendChild(w9g5p9.content.cloneNode(true));
         const thisSite = this.getAttribute("site"); 
@@ -268,31 +328,45 @@
             let randomSiteIndex = this.getRandomInt(0, sites.length - 1);
 
             let cp = `<span class="title">i'm a <a href="https://xandra.cc/safonts/" rel="external"><b>sa</b><i>font</i></a></span>`;
+
             cp += `<br/><span class="nav">`;
-            cp += `<a href="${sites[prevSiteIndex].url}" rel="external" aria-label="safonts webring: previous site">&lt;&lt;</a>`
+
+            cp += `<a href="${sites[prevSiteIndex].url}" rel="external" aria-label="safonts webring: previous site">&lt;&lt;</a>`;
+
             cp += `<a href="${sites[randomSiteIndex].url}" rel="external" aria-label="safonts webring: random site">???</a>`;
+
             cp += `<a href="https://xandra.cc/safonts/members.html" rel="external" aria-label="safonts webring: index">...</a>`;
+
             cp += `<a href="${sites[nextSiteIndex].url}" rel="external" aria-label="safonts webring: next site">&gt;&gt;</a></span>`;
 
             this.shadowRoot.querySelector(".safonts").insertAdjacentHTML("afterbegin", cp);
+
           });
 
           const sheet = new CSSStyleSheet;
+
           let style = `.safonts * { color: inherit; }`;
+
           style += `.safonts { margin: 1em auto 0px; padding-bottom: 1em; text-align: center; border-bottom: 1px solid rgb(138, 85, 138); }`;
+
           style += `.safonts .title a { font-size: larger; }`;
+
           sheet.replaceSync(style);
           this.shadowRoot.adoptedStyleSheets.push(sheet);
-      };
+
+      }
 
       getRandomInt(min, max) {
         this.min = Math.ceil(min);
         this.max = Math.floor(max);
         return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-      };
-    };
+      }
+
+    }
     // whatever 'customElement' you define MUST be unique
     // do not use the name 'webring-css' or you will conflict with other webrings
     window.customElements.define("ring-900", p1v1g6);
+
   };
+
 };
